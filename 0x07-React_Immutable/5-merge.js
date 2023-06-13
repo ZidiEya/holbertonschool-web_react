3784fb024192f360f96b9a3d0e672911ed809533
ZidiEya <91125384+ZidiEya@users.noreply.github.com>
2023-06-13 19:23:48 +0200

    function named concatElements  It accepts two arguments page1 and page2. Both are arrays It should return a List containing the values of the two pages

--- 0x07-React_Immutable/5-merge.js
+++ 0x07-React_Immutable/5-merge.js
@@ -1,11 +1,5 @@
-import { List, Map } from 'immutable';
+import { Map, List } from 'immutable';
 
-function concatElements(page1, page2) {
-    return List([...page1, ...page2]);
-}
+export const concatElements = (page1, page2) => List(page1).concat(List(page2));
 
-function mergeElements(page1, page2) {
-    return Map({...page1, ...page2});
-}
-
-export {concatElements, mergeElements}
+export const mergeElements = (page1, page2) => Map(page1).merge(Map(page2));
