f01b7cff8a65df44bc70f98d08a186a3df5e3157
ZidiEya <91125384+ZidiEya@users.noreply.github.com>
2023-06-13 19:20:36 +0200

    function named mergeDeeplyElements  It should accept two arguments, page1 and page2. Both are objects It should return a List containing the values of the two pages If two values are the same, they should combine each other

--- 0x07-React_Immutable/6-deeply.js
+++ 0x07-React_Immutable/6-deeply.js
@@ -1,6 +1,5 @@
-import { Map, toJS } from 'immutable';
+import { Map } from 'immutable';
 
-function mergeDeeplyElements(page1, page2) {
-    return Map(page1).mergeDeep(Map(page2));
-}
+const mergeDeeplyElements = (page1, page2) => Map(page1).mergeDeep(Map(page2));
+
 export default mergeDeeplyElements;
