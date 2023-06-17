47160de8116b2c079c3714f094d5c6f56d7bb5f8
ZidiEya <91125384+ZidiEya@users.noreply.github.com>
2023-06-17 05:21:00 +0200

    courseActionCreators.js

--- 0x08_react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.js
+++ 0x08_react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.js
@@ -1,3 +1,15 @@
-export const SELECT_COURSE = 'SELECT_COURSE';
+import { SELECT_COURSE, UNSELECT_COURSE } from './courseActionTypes';
 
-export const UNSELECT_COURSE = 'UNSELECT_COURSE';
+export const selectCourse = (index) => {
+  return {
+    type: SELECT_COURSE,
+    index,
+  };
+};
+
+export const unSelectCourse = (index) => {
+  return {
+    type: UNSELECT_COURSE,
+    index,
+  };
+};
