183d58aeb00f46063d69f7711c77f7bfd8296f5e
ZidiEya <91125384+ZidiEya@users.noreply.github.com>
2025-05-14 22:14:48 +0100

    Update App.js

--- react_redux_connectors_and_providers/dashboard/src/App/App.js
+++ react_redux_connectors_and_providers/dashboard/src/App/App.js
@@ -1,47 +1,62 @@
-// task_1/dashboard/src/App/App.js
-
 import React from 'react';
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Notifications from '../Notifications/Notifications';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
 import CourseList from '../CourseList/CourseList';
+import { displayNotificationDrawer, hideNotificationDrawer } from '../actions/uiActionCreators';
 
-// If you are using Immutable.js
-export const mapStateToProps = (state) => ({
-  isLoggedIn: state.get('isUserLoggedIn'),
-  displayDrawer: state.get('isNotificationDrawerVisible'),
-});
+export class App extends React.Component {
+  render() {
+    const {
+      isLoggedIn,
+      displayDrawer,
+      displayNotificationDrawer,
+      hideNotificationDrawer,
+    } = this.props;
 
-// If you're using plain JS objects instead, use this instead:
-// export const mapStateToProps = (state) => ({
-//   isLoggedIn: state.ui.isUserLoggedIn,
-//   displayDrawer: state.ui.isNotificationDrawerVisible,
-// });
-
-function App({ isLoggedIn, displayDrawer }) {
-  return (
-    <>
-      {displayDrawer && <Notifications />}
-      <div className="App">
-        <Header />
-        {isLoggedIn ? <CourseList /> : <Login />}
-        <Footer />
-      </div>
-    </>
-  );
+    return (
+      <>
+        <Notifications
+          displayDrawer={displayDrawer}
+          handleHideDrawer={hideNotificationDrawer}
+        />
+        <div className="App">
+          <Header handleDisplayDrawer={displayNotificationDrawer} />
+          {isLoggedIn ? <CourseList /> : <Login />}
+          <Footer />
+        </div>
+      </>
+    );
+  }
 }
 
+// Prop types
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
   displayDrawer: PropTypes.bool,
+  displayNotificationDrawer: PropTypes.func,
+  hideNotificationDrawer: PropTypes.func,
 };
 
 App.defaultProps = {
   isLoggedIn: false,
   displayDrawer: false,
+  displayNotificationDrawer: () => {},
+  hideNotificationDrawer: () => {},
 };
 
-export default connect(mapStateToProps)(App);
+export const mapStateToProps = (state) => ({
+  isLoggedIn: state.get('isUserLoggedIn'),
+  displayDrawer: state.get('isNotificationDrawerVisible'),
+});
+
+const mapDispatchToProps = {
+  displayNotificationDrawer,
+  hideNotificationDrawer,
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
