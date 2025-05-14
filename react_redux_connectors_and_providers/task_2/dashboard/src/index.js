202051a0c2436fe50a8e74c20c1cce323a958c03
ZidiEya <91125384+ZidiEya@users.noreply.github.com>
2025-05-14 23:39:54 +0100

    Create index.js

---
+++ react_redux_connectors_and_providers/task_2/dashboard/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App/App';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/uiReducer';
+
+// Use Redux DevTools extension compose if available, else fallback to default
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
