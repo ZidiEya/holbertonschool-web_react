9274856c11ef27883f662ced0b7b5188fba5698b
Eya Zidi <91125384+ZidiEya@users.noreply.github.com>
2025-07-10 16:48:47 +0100

    Delete react_intro directory

--- react_intro/task_3/dashboard/src/setupTests.js
+++
@@ -1,6 +0,0 @@
-// task_3/dashboard/src/setupTests.js
-import Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-
-// Configure Enzyme with the React 17 adapter
-Enzyme.configure({ adapter: new Adapter() });
